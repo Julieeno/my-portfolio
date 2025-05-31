21772dc7ba914acd95ec36979f940df7c960da26
Julieeno <giulianolazzara@gmail.com>
2025-05-31 18:46:38 +0200

    commit

--- src/main.ts
+++ src/main.ts
@@ -2,18 +2,18 @@ import { createApp } from 'vue'
 import { createI18n } from 'vue-i18n'
 import App from './App.vue'
 import './assets/style.css'
-import enMessages from './i18n/en.json'
-import itMessages from './i18n/it.json'
-import esMessages from './i18n/es.json'
+import en from './i18n/en.json'
+import it from './i18n/it.json'
+import es from './i18n/es.json'
 
 const i18n = createI18n({
   legacy: false,
   locale: 'en',
   fallbackLocale: 'en',
   messages: {
-    en: enMessages,
-    it: itMessages,
-    es: esMessages
+    en,
+    it,
+    es
   }
 })
 
